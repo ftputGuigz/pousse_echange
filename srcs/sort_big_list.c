ab17cbd8dc2fcfe8800ba38f484d67549f5ca4f9
Guillaume PETIT <gpetit@e2r9p7.42.fr>
2021-07-06 12:08:57 +0200

    determine the median in stack a

--- srcs/sort_big_list.c
+++ srcs/sort_big_list.c
@@ -12,7 +12,45 @@
 
 #include "push_swap.h"
 
-void	sort_big_list(t_stack *stack)
+int	*search_median(t_stack *stack)
 {
+	t_list	*lst;
+	int		*arr;
+	int		*median;
+	int		i;
+
+	i = 0;
+	lst = *(stack->a);
+	arr = malloc(sizeof(int) * ft_lstsize(lst));
+	if (!arr)
+		return (NULL);
+	while (lst)
+	{
+		arr[i++] = *((int *)lst->content);
+		lst = lst->next;
+	}
+	ft_simplesort(arr, i);
 	
+	median = malloc(sizeof(int));
+	if (!median)
+	{
+		free(arr);
+		return (NULL);
+	}
+	*median = arr[ft_lstsize(*(stack->a)) / 2];
+	free(arr);
+	return (median);
+}
+
+int	sort_big_list(t_stack *stack)
+{
+	int	*median;
+	
+	median = search_median(stack);
+	if (!median)
+		return (0);
+	printf("MEDIAN = %d\n", *median);
+	under_median()
+	free(median);
+	return (1);
 }
