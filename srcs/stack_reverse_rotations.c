4bc0cdc38b1b00ad95454b564ca370577f33ba08
Guillaume PETIT <gpetit@e3r3p7.42.fr>
2021-07-28 17:28:10 +0200

    retour d'erreurs des fonctions de mvmt

--- srcs/stack_reverse_rotations.c
+++ srcs/stack_reverse_rotations.c
@@ -21,7 +21,7 @@ t_list	*ft_lst_penultimate(t_list *lst)
 	return (lst);
 }
 
-void	rra(t_stack *stack)
+int	rra(t_stack *stack)
 {
 	int		size;
 	t_list	*penultimate;
@@ -30,20 +30,28 @@ void	rra(t_stack *stack)
 	void	*content;
 
 	if (!(*(stack->a)))
-		return ;
+		return (0);
 	size = ft_lstsize(*(stack->a));
 	if (size == 1)
-		return ;
+		return (1);
 	last = ft_lstlast(*(stack->a));
 	penultimate = ft_lst_penultimate(*(stack->a));
 	penultimate->next = NULL;
 	ft_lstadd_front(stack->a, last);
 	content = ft_strdup("rra");
+	if (!content)
+		return (0);
 	new_operation = ft_lstnew(content);
+	if (!new_operation)
+	{
+		free(content);
+		return (0);
+	}
 	ft_lstadd_back(stack->output, new_operation);
+	return (1);
 }
 
-void	rrb(t_stack *stack)
+int	rrb(t_stack *stack)
 {
 	int		size;
 	t_list	*penultimate;
@@ -52,17 +60,25 @@ void	rrb(t_stack *stack)
 	void	*content;
 
 	if (!(*(stack->b)))
-		return ;
+		return (0);
 	size = ft_lstsize(*(stack->b));
 	if (size == 1)
-		return ;
+		return (1);
 	last = ft_lstlast(*(stack->b));
 	penultimate = ft_lst_penultimate(*(stack->b));
 	penultimate->next = NULL;
 	ft_lstadd_front(stack->b, last);
 	content = ft_strdup("rrb");
+	if (!content)
+		return (0);
 	new_operation = ft_lstnew(content);
+	if (!new_operation)
+	{
+		free(content);
+		return (0);
+	}
 	ft_lstadd_back(stack->output, new_operation);
+	return (1);
 }
 
 void	rrr(t_stack *stack)
